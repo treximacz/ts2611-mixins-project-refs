b0b5af77ef88369ead7d327bf45d034ad51eee61
Richard Skopal <skopalr@trexima.cz>
2021-09-14 16:56:59 +0200

    Delete build files

--- mixins/lib/EventsSubscriber.d.ts
+++
@@ -1,8 +0,0 @@
-declare type Constructor<T = {}> = new (...args: any[]) => T;
-export declare function EventsSubscriber<T extends Constructor<{}>>(Base: T): {
-    new (...args: any[]): {
-        readonly eventsFilter: string[];
-        handleEvent(event: string): void;
-    };
-} & T;
-export {};
